b5e5bf416e02c3d47698e8c4f624a3834c2854ca
cyjseagull <yujiechen_hust@163.com>
2019-07-28 00:36:44 +0800

    format the code (#20)
    * format the code
    
    * fix misspell
    
    * fix code according to flake8
    
    * update README

--- client/stattool.py
+++ client/stattool.py
@@ -1,24 +1,29 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 '''
-  bcosliteclientpy is a python client for FISCO BCOS2.0 (https://github.com/FISCO-BCOS/FISCO-BCOS)
-  bcosliteclientpy is free software: you can redistribute it and/or modify it under the terms of the MIT License as published by the Free Software Foundation
-  This project is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE
-  Thanks for authors and contributors of eth-abi，eth-account，eth-hash，eth-keys，eth-typing，eth-utils，rlp, eth-rlp , hexbytes ...and relative projects
+  bcosliteclientpy is a python client for FISCO BCOS2.0 (https://github.com/FISCO-BCOS/)
+  bcosliteclientpy is free software: you can redistribute it and/or modify it under the
+  terms of the MIT License as published by the Free Software Foundation. This project is
+  distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. Thanks for
+  authors and contributors of eth-abi, eth-account, eth-hash，eth-keys, eth-typing, eth-utils,
+  rlp, eth-rlp , hexbytes ... and relative projects
   @author: kentzhang
   @date: 2019-06
 '''
 from client import clientlogger
 import time
+
+
 class StatTool:
     time_begin = time.time()
-    time_end  = 0
+    time_end = 0
     time_used = 0
     unit = "ms"
     @staticmethod
     def begin(unit="ms"):
-        stat =  StatTool()
-        stat.time_begin =time.time()
+        stat = StatTool()
+        stat.time_begin = time.time()
         stat.unit = unit
         return stat
 
@@ -26,22 +31,22 @@ class StatTool:
         self.time_end = time.time()
         self.time_used = self.time_end - self.time_begin
 
-    def make_statmsg(self,msg):
-        if self.time_end ==0 :
+    def make_statmsg(self, msg):
+        if self.time_end == 0:
             self.done()
-        timeused_toshow  = self.time_used
+        timeused_toshow = self.time_used
 
-        if self.unit=="ms":
-            timeused_toshow = timeused_toshow *1000
+        if self.unit == "ms":
+            timeused_toshow = timeused_toshow * 1000
 
-        statmsg  = "%.3f%s,%s"% (timeused_toshow,self.unit,msg)
+        statmsg = "%.3f%s,%s" % (timeused_toshow, self.unit, msg)
         return statmsg
 
-    def debug(self,msg):
+    def debug(self, msg):
         clientlogger.statlogger.debug(self.make_statmsg(msg))
 
-    def info(self,msg):
+    def info(self, msg):
         clientlogger.statlogger.info(self.make_statmsg(msg))
 
-    def error(self,msg):
+    def error(self, msg):
         clientlogger.statlogger.info(self.make_statmsg(msg))
--- demo_get.py
+++ demo_get.py
@@ -2,10 +2,13 @@
 # -*- coding: utf-8 -*-
 
 '''
-  bcosliteclientpy is a python client for FISCO BCOS2.0 (https://github.com/FISCO-BCOS/FISCO-BCOS)
-  bcosliteclientpy is free software: you can redistribute it and/or modify it under the terms of the MIT License as published by the Free Software Foundation
-  This project is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE
-  Thanks for authors and contributors of eth-abi，eth-account，eth-hash，eth-keys，eth-typing，eth-utils，rlp, eth-rlp , hexbytes ...and relative projects
+  bcosliteclientpy is a python client for FISCO BCOS2.0 (https://github.com/FISCO-BCOS/)
+  bcosliteclientpy is free software: you can redistribute it and/or modify it under the
+  terms of the MIT License as published by the Free Software Foundation. This project is
+  distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. Thanks for
+  authors and contributors of eth-abi, eth-account, eth-hash，eth-keys, eth-typing, eth-utils,
+  rlp, eth-rlp , hexbytes ... and relative projects
   @author: kentzhang
   @date: 2019-06
 '''
@@ -15,22 +18,19 @@ from client.bcosclient import (
     BcosError
 )
 from client.stattool import StatTool
-import os
-from eth_utils import to_checksum_address
 from client.datatype_parser import DatatypeParser
-import time
 client = BcosClient()
 info = client.getinfo()
-print("client info:",info)
+print("client info:", info)
 
 
-#从文件加载abi定义
-abi_file  ="contracts/SimpleInfo.abi"
+# 从文件加载abi定义
+abi_file = "contracts/SimpleInfo.abi"
 data_parser = DatatypeParser()
 data_parser.load_abi_file(abi_file)
 contract_abi = data_parser.contract_abi
 
-#以下是查询类的接口，大部分是返回json，可以根据对fisco bcos rpc接口json格式的理解，进行字段获取和转码
+# 以下是查询类的接口，大部分是返回json，可以根据对fisco bcos rpc接口json格式的理解，进行字段获取和转码
 '''
 useful helper:
 int(num,16)  hex -> int
@@ -41,60 +41,61 @@ stat = StatTool.begin()
 print("\n>>---------------------------------------------------------------------")
 res = client.getNodeVersion()
 print("\n>>---------------------------------------------------------------------")
-print("getClientVersion",res)
+print("getClientVersion", res)
 print("\n>>---------------------------------------------------------------------")
 try:
     res = client.getBlockNumber()
-    print("getBlockNumber",res)
+    print("getBlockNumber", res)
 except BcosError as e:
-    print("bcos client error,",e.info())
+    print("bcos client error,", e.info())
 print("\n>>---------------------------------------------------------------------")
-print("getPeers",client.getPeers())
+print("getPeers", client.getPeers())
 print("\n>>---------------------------------------------------------------------")
-print("getBlockByNumber",client.getBlockByNumber(1))
+print("getBlockByNumber", client.getBlockByNumber(1))
 print("\n>>---------------------------------------------------------------------")
 blockhash = client.getBlockHashByNumber(1)
-print("getBlockHashByNumber",blockhash)
+print("getBlockHashByNumber", blockhash)
 print("\n>>---------------------------------------------------------------------")
 block = client.getBlockByHash(blockhash)
-print("getBlockByHash",block)
-txhash =block['transactions'][0]
+print("getBlockByHash", block)
+txhash = block['transactions'][0]
 print("\n>>---------------------------------------------------------------------")
-print("getTransactionByHash",client.getTransactionByHash(txhash))
+print("getTransactionByHash", client.getTransactionByHash(txhash))
 print("\n>>---------------------------------------------------------------------")
-print("getTransactionByBlockHashAndIndex",client.getTransactionByBlockHashAndIndex(blockhash,0))
+print("getTransactionByBlockHashAndIndex", client.getTransactionByBlockHashAndIndex(blockhash, 0))
 print("\n>>---------------------------------------------------------------------")
-print("getTransactionByBlockNumberAndIndex",client.getTransactionByBlockNumberAndIndex(1,0))
+print("getTransactionByBlockNumberAndIndex", client.getTransactionByBlockNumberAndIndex(1, 0))
 print("\n>>---------------------------------------------------------------------")
-print("getTransactionReceipt",client.getTransactionReceipt(txhash))
+print("getTransactionReceipt", client.getTransactionReceipt(txhash))
 print("\n>>---------------------------------------------------------------------")
-print("getPendingTransactions",client.getPendingTransactions())
+print("getPendingTransactions", client.getPendingTransactions())
 print("\n>>---------------------------------------------------------------------")
-print("getPendingTxSize",client.getPendingTxSize())
+print("getPendingTxSize", client.getPendingTxSize())
 print("\n>>---------------------------------------------------------------------")
-print("getCode",client.getCode("0x83592a3cf1af302612756b8687c8dc7935c0ad1d"))
+print("getCode", client.getCode("0x83592a3cf1af302612756b8687c8dc7935c0ad1d"))
 print("\n>>---------------------------------------------------------------------")
-print("getTotalTransactionCount",client.getTotalTransactionCount())
+print("getTotalTransactionCount", client.getTotalTransactionCount())
 print("\n>>---------------------------------------------------------------------")
-print("getSystemConfigByKey",client.getSystemConfigByKey("tx_count_limit"))
+print("getSystemConfigByKey", client.getSystemConfigByKey("tx_count_limit"))
 print("\n>>---------------------------------------------------------------------")
 
-print("getPbftView",int(client.getPbftView(),16))
+print("getPbftView", int(client.getPbftView(), 16))
 print("\n>>---------------------------------------------------------------------")
-print("getSealerList",client.getSealerList())
+print("getSealerList", client.getSealerList())
 print("\n>>---------------------------------------------------------------------")
-print("getObserverList",client.getObserverList())
+print("getObserverList", client.getObserverList())
 print("\n>>---------------------------------------------------------------------")
-print("getConsensusStatus",client.getConsensusStatus())
+print("getConsensusStatus", client.getConsensusStatus())
 print("\n>>---------------------------------------------------------------------")
-print("getSyncStatus",client.getSyncStatus())
+print("getSyncStatus", client.getSyncStatus())
 print("\n>>---------------------------------------------------------------------")
-print("getGroupPeers",client.getGroupPeers())
+print("getGroupPeers", client.getGroupPeers())
 print("\n>>---------------------------------------------------------------------")
-print("getNodeIDList",client.getNodeIDList())
+print("getNodeIDList", client.getNodeIDList())
 print("\n>>---------------------------------------------------------------------")
-print("getGroupList",client.getGroupList())
+print("getGroupList", client.getGroupList())
 stat.done()
 reqcount = next(client.request_counter)
-print("demo get finish, total request {},usedtime {},avgtime:{}".format(reqcount,stat.time_used,(stat.time_used/reqcount)))
+print("demo get finish, total request {},usedtime {},avgtime:{}".format(
+    reqcount, stat.time_used, (stat.time_used / reqcount)))
 client.finish()
