36782838dd066da8275dc6e18ec97d6710454787
cyjseagull <1547158861@qq.com>
2019-07-29 02:46:58 +0800

    add cns/consensus/sys_config/permission/crud precompile

---
+++ client/precompile/consensus/consensus_precompile.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+  bcosliteclientpy is a python client for FISCO BCOS2.0 (https://github.com/FISCO-BCOS/)
+  bcosliteclientpy is free software: you can redistribute it and/or modify it under the
+  terms of the MIT License as published by the Free Software Foundation. This project is
+  distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. Thanks for
+  authors and contributors of eth-abi, eth-account, eth-hash，eth-keys, eth-typing, eth-utils,
+  rlp, eth-rlp , hexbytes ... and relative projects
+  @file: consensus_precompile.py
+  @function:
+  @author: yujiechen
+  @date: 2019-07
+'''
+from client.common import transaction_common
+
+
+class ConsensusPrecompile:
+    """
+    implementation of ConsensusPrecompile
+    """
+
+    def __init__(self, contract_path):
+        """
+        init the address for Consensus contract
+        """
+        self._consensus_address = "0x0000000000000000000000000000000000001003"
+        self._consensus_abi_path = contract_path + "/Consensus.abi"
+        self.client = transaction_common.TransactionCommon(self._consensus_address)
+
+    def __del__(self):
+        """
+        finish the client
+        """
+        self.client.finish()
+
+    def addSealer(self, nodeId):
+        """
+        addSealer
+        """
+        fn_name = "addSealer"
+        fn_args = [nodeId]
+        return self.client.send_transaction_getReceipt(self._consensus_abi_path, fn_name, fn_args)
+
+    def addObserver(self, nodeId):
+        """
+        addObserver
+        """
+        fn_name = "addObserver"
+        fn_args = [nodeId]
+        return self.client.send_transaction_getReceipt(self._consensus_abi_path, fn_name, fn_args)
+
+    def removeNode(self, nodeId):
+        """
+        remove Node
+        """
+        fn_name = "remove"
+        fn_args = [nodeId]
+        return self.client.send_transaction_getReceipt(self._consensus_abi_path, fn_name, fn_args)
