a4d177e149adc5077ba8e92067e34829acd2cd37
Ma liang <42969779+tenthousands@users.noreply.github.com>
2023-03-08 15:53:03 +0800

    Update console.py (#190)
    Co-authored-by: kentzhang <20529321+coderkentzhang@users.noreply.github.com>

--- console.py
+++ console.py
@@ -18,12 +18,13 @@ python sdk 适配FISCO BCOS 2.x/3.x版本，控制台的实现文件做了分离
 
 README文档的命令行示例里未做版本区分，仅供演示参考，实际使用时：
 
-* 如面向FISCO BCOS2.x，把console.py换成console2.py。如: python consle2.py getBlockNumber
 
-* 如面向FISCO BCOS3.x，把console.py换成console3.py。如: python consle3.py getBlockNumber
+* 如使用FISCO BCOS2.x的控制台，把console.py换成console2.py。如: python console2.py getBlockNumber
+
+* 如使用FISCO BCOS3.x的控制台，把console.py换成console3.py。如: python console3.py getBlockNumber
 
 demo目录里的demo_get2/3, demo_transaction2/3的逻辑与此类似。
 
-'''
+
 
 print(hint)
